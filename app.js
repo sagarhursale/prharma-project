602eb145173ad6f89602e357dcadd042241850a2
sagarhursale <rsagar.hursale143@gmail.com>
2025-10-07 21:48:45 +0530

    This is my latest commit

---
+++ app.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Basic JavaScript Example</title>
+</head>
+<body>
+
+  <h2>Click the Button</h2>
+  <button onclick="showMessage()">Click Me!</button>
+
+  <script>
+    function showMessage() {
+      alert("Hello! You clicked the button.");
+    }
+  </script>
+
+</body>
+</html>
